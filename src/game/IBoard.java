24cfa5945f48df7dc94b7b3da98da504ad6c9fac
godiale <godin_a_e@mail.ru>
2017-02-23 23:33:42 +0300

    -- progressing with implementation of a board

--- src/game/IBoard.java
+++ src/game/IBoard.java
@@ -5,7 +5,7 @@ public interface IBoard {
     public int getXSize();
     public int getYSize();
 
-    public void placeShip(int i, int j);
+    public void placeShip(IShip ship, int i, int j);
     public Answer checkStrike(Strike strike);
     public void recordStrike(Strike strike);
 }
--- src/game/SquareBoard.java
+++ src/game/SquareBoard.java
@@ -1,11 +1,25 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SquareBoard implements IBoard {
 
     private int size;
 
+    private List<IShip> ships2coord; // with real board coordinates
+    private int [][] coord2ships;    // index in ships2coord, -1 if no ship
+    private int [][] strikesCount;   // number of strikes to the point
+
     public SquareBoard(int size) {
         this.size = size;
+        ships2coord = new ArrayList<IShip>();
+        coord2ships = new int[size][size];
+        for (int[] row: coord2ships) {
+            Arrays.fill(row, -1);
+        }
+        strikesCount = new int[size][size];
     }
 
     @Override
@@ -19,19 +33,33 @@ public class SquareBoard implements IBoard {
     }
 
     @Override
-    public void placeShip(int i, int j) {
-        // TODO Auto-generated method stub
+    public void placeShip(IShip ship, int i, int j) {
+
+        class PlacedShip implements IShip {
+            private List<Point> points;
+            public PlacedShip(IShip ship, int i, int j) {
+                points = new ArrayList<Point>();
+                ship.getCoord().stream().forEach(p -> points.add(new Point(p.x+i, p.y+j)));
+            }
+            @Override
+            public List<Point> getCoord() {
+                return points;
+            }
+        }
+
+        IShip placedShip = new PlacedShip(ship, i, j);
+        final int shipIdx = ships2coord.size();
+        ships2coord.add(placedShip);
+        placedShip.getCoord().stream().forEach(p -> coord2ships[p.x][p.y] = shipIdx);
     }
 
     @Override
     public Answer checkStrike(Strike strike) {
-        // TODO Auto-generated method stub
-        return null;
+        return Answer.MISS;
     }
 
     @Override
     public void recordStrike(Strike strike) {
-        // TODO Auto-generated method stub
-
+        strikesCount[strike.x][strike.y]++;
     }
 }
